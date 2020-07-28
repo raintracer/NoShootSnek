9727d06c63b7c558fd6d03feae69512e713e0c08
Unknown <rts@rtstevenson.com>
2020-07-28 17:12:55 -0400

    Created Basic Game Menu

---
+++ Assets/Scripts/MenuController.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuController : MonoBehaviour
+{
+
+    void PlayGame()
+    {
+        SceneManager.LoadScene(1);
+    }
+    
+}

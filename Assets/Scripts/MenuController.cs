58481265595584946284d316ead9701ffcd9745b
Unknown <rts@rtstevenson.com>
2020-07-30 15:00:41 -0400

    Changed Menu to incorporate "Press Any Key" to play and added blinking effect to assocaited text.

--- Assets/Scripts/MenuController.cs
+++ Assets/Scripts/MenuController.cs
@@ -1,14 +1,51 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 using UnityEngine.SceneManagement;
+using UnityEngine.XR.WSA.Input;
 
 public class MenuController : MonoBehaviour
 {
 
+    GameObject AnyKeyTextObject;
+
+    public void Awake()
+    {
+        AnyKeyTextObject = GameObject.Find("Play Button Text");
+        if (AnyKeyTextObject == null)
+        {
+            Debug.LogError("Play Button Text not found");
+        }
+    }
+
+    public void Start()
+    {
+        StartCoroutine("BlinkPlayText");
+    }
+
+    IEnumerator BlinkPlayText()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(1);
+            AnyKeyTextObject.SetActive(false);
+            yield return new WaitForSeconds(1);
+            AnyKeyTextObject.SetActive(true);
+        }
+    }
+
+    void Update()
+    {
+        if (Keyboard.current.anyKey.wasPressedThisFrame)
+        {
+            SceneManager.LoadScene(1);
+        }
+    }
+
     void PlayGame()
     {
-        SceneManager.LoadScene(1);
+        // SceneManager.LoadScene(1);
     }
-    
+
 }
